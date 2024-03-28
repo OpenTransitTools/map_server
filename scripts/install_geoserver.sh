0ff860b5d5a6a44ceefc26503ddd74ff40fd63df
Frank Purcell <fxpurcell@gmail.com>
2024-03-27 17:48:55 -0700

    fix: more info to script

--- scripts/install_geoserver.sh
+++ scripts/install_geoserver.sh
@@ -43,12 +43,19 @@ rm -rf geoserver/data_dir
 # see: https://gis.stackexchange.com/questions/402613/layer-preview-not-working-at-all-on-geoserver
 rm geoserver/webapps/geoserver/WEB-INF/lib/marlin-0.9.3.jar
 
+echo
 echo "**** DO THIS TO GET geoserver UP & RUNNING ****"
+echo
+echo bin/generate_geoserver_config -db localhost
 echo
 echo "bin/run.sh &"
 echo "    OR"
 echo "export GEOSERVER_DATA_DIR=$PWD/geoserver/data"
 echo "cd geoserver"
 echo "nohup bin/startup.sh > logs/run.out &"
+echo
+echo mbtile preview:
+echo http://localhost:8080/geoserver/gwc/demo/routes_n_stops
 echo 
 echo "***********************************************"
+echo
