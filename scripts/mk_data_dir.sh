552bfe75addc12445c71ea233d859581e1d9d913
Frank Purcell <fxpurcell@gmail.com>
2024-09-26 16:57:55 -0700

    fix: bit more docs in mk data_dir

--- scripts/mk_data_dir.sh
+++ scripts/mk_data_dir.sh
@@ -1,5 +1,14 @@
 DIR=`dirname $0`
 . $DIR/base.sh
+git pull
 $DIR/download_clean_data_dir.sh
+bc=`find ${DIR}/../data_dir | wc -l`
 bin/get_agencies
 bin/generate_geoserver_config
+ac=`find ${DIR}/../data_dir | wc -l`
+
+if [ $ac -gt $bc ]; then
+  echo "success: data_dir has config with $ac files (blank dd has $bc)"
+else
+  echo "fail: data_dir $bc v. $ac files"
+fi
