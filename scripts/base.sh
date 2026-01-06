0b6054ef860f5f853e61dea0d0b1a277c5cf3fba
Frank Purcell <fxpurcell@gmail.com>
2026-01-05 19:53:51 -0800

    fix: move to poetry ... partial update of scripts to clone fresh data_dir

--- scripts/base.sh
+++
@@ -1,8 +0,0 @@
-RTP_DIR=~/rtp
-MS_DIR=$RTP_DIR/map_server
-NEW_DATA_DIR=$MS_DIR/data_dir
-GTFS_DIR=~/gtfs
-
-DATA_REPO="geoserver_data_dir"
-DATA_REPO_URL="https://github.com/OpenTransitTools/${DATA_REPO}.git"
-DATA_REPO_DIR="$RTP_DIR/$DATA_REPO"
